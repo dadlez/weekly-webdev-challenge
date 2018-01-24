9197ecd560327f76e9622e8cb8ff643c76594a69
Łukasz Dadlez <lukaszdadlez@gmail.com>
2018-01-24 22:53:05 +0100

    added scss loader to webpack - not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,31 @@
+const path = require('path');
+
 module.exports = {
-  entry: "src/index.scss"
-}
+  entry: ['./src/index.js', './src/index.scss'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'index.css',
+              outputPath: 'dist/'
+            }
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
